77660d8a30a511a5849def4694fb9c8378ca4232
rizki <rizkiadnanfutuh@gmail.com>
2025-09-07 19:35:33 +0700

    feat(docs): add Swagger UI and autogen setup for API documentation

---
+++ src/docs/swagger.ts
@@ -0,0 +1,39 @@
+import swaggerAutogen from "swagger-autogen";
+
+const outputFile = "./swagger_output.json";
+
+const endpointsFiles = ["../routes/api.ts"];
+
+const doc = {
+  info: {
+    version: "v0.0.1",
+    title: "Dokumentasi Api ACARA",
+    description: "Dokumentasi Api ACARA",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "local Server",
+    },
+    {
+      url: "https://back-end-acara-gamma-ten.vercel.app/",
+      description: "Deploy Server",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      loginRequest: {
+        identifier: "Adnan Progammer",
+        password: "password123",
+      },
+    },
+  },
+};
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import router from "./routes/api";
 import db from "./utils/database";
+import docs from "./docs/route";
 
 async function init() {
   try {
@@ -9,11 +11,15 @@ async function init() {
     console.log("Database Status: ", result);
     const app = express();
 
+    app.use(cors());
+
     app.use(bodyParser.json());
 
     const PORT = 3000;
 
     app.use("/api", router);
+
+    docs(app);
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
