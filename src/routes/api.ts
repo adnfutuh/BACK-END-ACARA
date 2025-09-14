e0ddbb8c48465341323a17caeee20f4b4165421d
rizki <rizkiadnanfutuh@gmail.com>
2025-09-15 01:39:27 +0700

    feat(acl): add role-based access control middleware and protect test-acl route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import authController from "../controllers/auth.controller";
 import authMiddleware from "../middlewares/auth.middleware";
+import aclMiddleware from "../middlewares/acl.middleware";
+import { ROLES } from "../utils/constant";
 
 const router = express.Router();
 
@@ -11,5 +13,13 @@ router.post("/auth/login", authController.login);
 router.get("/auth/me", authMiddleware, authController.me);
 
 router.post("/auth/activation", authController.activation);
+
+router.get(
+  "/test-acl",
+  [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
+  (req: Request, res: Response) => {
+    res.status(200).json({ data: "Sucess", message: "Ok" });
+  }
+);
 
 export default router;
